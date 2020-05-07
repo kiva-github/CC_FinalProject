155718096380efc6591dbaf2de8c6a2ae35f4e83
kiva-github <kivaloor@gmail.com>
2020-05-06 18:50:10 -0700

    Inheritance Push
    added inheritance + more

--- sketch874247/mySketch.js
+++ sketch874247/mySketch.js
@@ -16,9 +16,6 @@ let phoenixJSON;
 let denver;
 let denverJSON;
 
-// los%20angeles
-// Seattle
-
 function preload(){
 	img = loadImage('http://www.openprocessing.org/sketch/874247/files/usa_outline.png');
   seattleJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=seattle&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
@@ -28,6 +25,12 @@ function preload(){
 	chicagoJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=chicago&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
 	phoenixJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=phoenix&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
 	denverJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=denver&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
+	philadelphiaJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=philadelphia&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
+	houstonJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=houston&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
+
+
+
+
 }
 
 function setup() {
@@ -40,35 +43,34 @@ function setup() {
 	cityArray.push(new City(chicagoJSON, 712, 280));
 	cityArray.push(new City(phoenixJSON, 295, 488));
 	cityArray.push(new City(denverJSON, 456, 378));
+	cityArray.push(new City(philadelphiaJSON, 917, 286));
+	cityArray.push(new City(houstonJSON, 635, 551));
+
+
 }
 
 function draw() {
 	image(img, 0, 0, width, height);
 	for (i = 0; i < cityArray.length; i++){
 		cityArray[i].display();
+		if (cityArray[i].isActive == true){
+			cityArray[i].displayWeather();
+		}
 	}
 }
-
 
 function mousePressed(){
-	print("X: "+ mouseX + " Y: " + mouseY);
-}
-
-function printLocation(x, y){
-	if ((x > 1131/2) && (y > 724/2)) {
-		print("Bottom Right");
-	} else if ((x > 1131/2) && (y < 724/2)) {
-		print("Top Right");
-	} else if ((x < 1131/2) && (y > 724/2)) {
-		print("Bottom Left");
-	} else {
-		print("Top Left");
+	for (i = 0; i < cityArray.length; i++){
+		cityArray[i].mouseClick();
 	}
+	print(mouseX, mouseY);
 }
 
+// CLASSES //
 
-class City {
-	constructor(cityJSON, xCoord, yCoord){
+class Location {
+	constructor(cityJSON, xCoord, yCoord) {
+		this.test = 5;
 		this.name = cityJSON.name;
 		this.x = xCoord;
 		this.y = yCoord;
@@ -76,62 +78,92 @@ class City {
 		this.temp = cityJSON.main.temp;
 		this.windSpeed = cityJSON.wind.speed;
 		this.weather = cityJSON.weather[0].description;
-	}
+		this.isActive = false;
+		this.displayCase = "";
+  }
 	
-	display(){
-		
-		print(this.temp > 100);
-		
-		// switch (this.temp) {
-		// 	case (this.temp > 100):
-		// 		fill(255,0,0);
-		// 		break;
-		// 	case (this.temp > 90):
-		// 		fill(117, 25, 25);
-		// 		break;
-		// 	case (this.temp > 80):
-		// 		fill(168, 35, 35);
-		// 		break;
-		// 	case (this.temp > 70):
-		// 		fill(227, 110, 14);
-		// 		break;
-		// 	case (this.temp > 60):
-		// 		fill(227, 167, 14);
-		// 		break;
-		// 	case (this.temp > 50):
-		// 		fill(255, 236, 31);
-		// 		break;
-		// 	case (this.temp > 40):
-		// 		fill(7, 179, 39); 
-		// 		break;
-		// 	case (this.temp > 30):
-		// 		fill(34, 163, 189); 
-		// 		break;
-		// 	case (this.temp > 20):
-		// 		fill(22, 110, 145); 
-		// 		print("> 20");
-		// 		break;
-		// 	case (this.temp > 10):
-		// 		fill(13, 53, 112); 
-		// 		break;
-		// 	case (this.temp > 0):
-		// 		fill(113, 45, 168); 
-		// 		break;
-		// }
-		
-		switch(this.temp){
-			case (this.temp < 80):
-				print("LEss than 80");
-				break;
-			case (this.temp > 80):
-				print("More than 80");
-				break;
+	mouseClick(){
+		if (dist(this.x, this.y, mouseX, mouseY) < 12) {
+			if (this.isActive == false) {
+				this.isActive = true;
+			}
+		} else {
+			this.isActive = false;
 		}
-			
-		ellipse(this.x, this.y, 25, 25);		
+	}
+		
+	displayWeather(){
+		
+				
 		text("City: " + this.name, this.x - 10, this.y + 25);
 		text("Temp: " + this.temp, this.x - 10, this.y + 45);
 		text("Forecast: " + this.weather, this.x - 10, this.y + 65);
 		text("Wind: " + this.windSpeed + "mph", this.x - 10, this.y + 85);
+		
+		if (this.weather == "clear sky"){
+			print(this.name + "'s weather is clear sky");
+		} else if (this.weather == "scattered clouds"){
+			print(this.name + "'s weather is scattered clouds");
+			this.scatteredClouds();
+		} else if (this.weather == "broken clouds"){
+			print(this.name + "'s weather is broken clouds");
+		} else if (this.weather == "overcast clouds"){
+			print(this.name + "'s weather is overcast clouds");
+		} else if (this.weather == "few clouds"){
+			print(this.name + "'s weather is few clouds");
+		} else if (this.weather == "light rain"){
+			print(this.name + "'s weather is light rain");
+		} else {
+			print(this.name + "'s weather is undefined");
+		}
+	}
+	
+	scatteredClouds(){
+
+		noStroke();
+		fill(255);
+		ellipse(50,50,60,50);
+		ellipse(80,40,60,50);
+		ellipse(130,50,60,50);
+		ellipse(70,70,60,50);
+		ellipse(110,65,60,50);
+	}
+	
+	
+	
+}
+	
+
+class City extends Location {
+	constructor(cityJSON, xCoord, yCoord){
+		super(cityJSON, xCoord, yCoord);																									
+	}
+	
+	display(){
+		
+		if (this.temp > 100){
+			fill("#FC582A")
+		} else if (this.temp > 80) {
+			fill("#FF9627")
+		} else if (this.temp > 70) {
+			fill("#FDC130")
+		} else if (this.temp > 60) {
+			fill("#8CC051")
+		} else if (this.temp > 50) {
+			fill("#319928")
+		} else if (this.temp > 40) {
+			fill("#139588")
+		} else if (this.temp > 30) {
+			fill("#23BBCD")
+		} else if (this.temp > 20) {
+			fill("#1DA9F0")
+		} else if (this.temp > 10) {
+			fill("#557EF8")
+		} else {
+			fill("#3E57AF")
+		}
+			
+		ellipse(this.x, this.y, 25, 25);
+		
 	}
 }
