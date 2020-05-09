0167d429d34e5afb65fdd052eb69cd91e9702720
kiva-github <kivaloor@gmail.com>
2020-05-09 12:52:09 -0700

    Finished Project V1 Push
    working project, will push again with final touch-ups

--- sketch874247/mySketch.js
+++ sketch874247/mySketch.js
@@ -1,19 +1,21 @@
-let countryOutline;
-var skyColor;
-var cityArray = [];
-let fastDropArray = [];
-let cloudArray = [];
-let snowflakeArray = [];
-let lightningTimer = 2;
+var skyColor; // global variable for manipulating background color
+var cityArray = []; // array for holding all City objects
+let fastDropArray = []; // array for holding all Raindrop objects for rain behaviours
+let cloudArray = []; // array for holding all Cloud objects for cloud behaviours
+let snowflakeArray = []; // array for holding all Snowflake objects for snow behaviour
+let lightningTimer = 2; // variable for timing in between lightning flashes for thunderstorm behaviour
 
-var weatherNames = {};
-let weatherNameLibrary;
-let buttonArray = []
+var weatherNames = {}; // dictionary to hold all weather descriptions and number of occurances (i.e. "clear sky": 7)
+let weatherNameLibrary; // array for holding all known weather descriptions
+let buttonArray = [] // array for holding all weather buttons
 
-let weatherClassObj;
+let weatherClassObj; // instantiation of object of type Weather in order to call its various weather methods. I tried looking
+										 // into making this class an abstract class because I am just calling its methods, but could not find any
+										 // realiable sources
 
-var activeCity;
+var activeCity; // variable that makes it so cities so up on load
 
+// declaration of all City objects (all state capitals, for the most part) and their JSON variables
 let atlanta, atlantaJSON;
 let augusta, augustaJSON;
 let austin, austinJSON;
@@ -58,9 +60,10 @@ let seattle, seattleJSON;
 let topeka, topekaJSON;
 
 function preload(){
-	img = loadImage('http://www.openprocessing.org/sketch/874247/files/usa_outline.png');
-	weatherClassObj = new Weather();
+	img = loadImage('http://www.openprocessing.org/sketch/874247/files/usa_outline.png'); // image of US, edited in Photoshop to have no fill
+	weatherClassObj = new Weather(); // instantiation of Weather object for calling class methods, as descibed above
 	
+	// loading JSON files for all cities
 	atlantaJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=atlanta&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
 	augustaJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=augusta&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
 	austinJSON = loadJSON("https://api.openweathermap.org/data/2.5/weather?q=austin&units=imperial&APPID=e812164ca05ed9e0344b89ebe273c141");
@@ -108,13 +111,16 @@ function preload(){
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(255);
-	skyColor = color(125);
-	activeCity = 0;
+	skyColor = color(125); // sky set to gray by default
+	activeCity = 0; // variable that makes it so cities show on load
 	
+	// all known weather descriptions. this is probably not the complete list, just what was the weather in the US during the time that I
+	// creating this program, which is why there are no winter descriptions
 	weatherNameLibrary = ["clear sky", "scattered clouds", "broken clouds", "overcast clouds", "few clouds",
 												"light rain", "shower rain", "very heavy rain", "heavy intensity rain", "haze", "mist",
 												"snow", "moderate rain", "thunderstorm with rain", "light intensity drizzle"];
 	
+	// instantiate and add all City objects to the cityArray
 	cityArray.push(new City(atlantaJSON, 817, 438));
 	cityArray.push(new City(augustaJSON, 998, 106));
 	cityArray.push(new City(austinJSON, 572, 549));
@@ -158,16 +164,20 @@ function setup() {
 	cityArray.push(new City(seattleJSON, 179, 140));
 	cityArray.push(new City(topekaJSON, 591, 346));
 	
-	// Add Raindrop objects to respective array based on rain intensity
+	// add Raindrop objects to respective array based on rain intensity
 	for (i = 0; i < 500; i++){
 		fastDropArray[i] = new Raindrop();
 	}
 	
-	// Add Cloud objects to respective array based on cloud type
+	// add Cloud objects to respective array based on cloud type
 	for (i = 0; i < 20; i++){
 		cloudArray[i] = new Cloud();
 	}
 	
+	// look through all City objects, check if the weather description is already in the dictionary: if it is, increment number
+	// of occurances by 1, if it isn't, add it to the dictionary. this code ensures that even if there is a weather description
+	// that isn't in my array of known descriptions (the weatherNameLibrary array), there will still be a button created for that
+	// type of weather (even though i would not have made an animation for it).
 	for (i = 0; i < cityArray.length; i++){
 		if (weatherNames[cityArray[i].weather] == undefined) {
 			weatherNames[cityArray[i].weather] = 1;
@@ -176,6 +186,9 @@ function setup() {
 		}
 	}
 	
+	// look through array of all known weather descriptions, add to dictionary if it isn't ready there, otherwise increment number of
+	// occurances. this code ensures that any weather that I did create an animation for is included and can be viewed by using the
+	// weather buttons on the right side of the screen
 	for (i = 0; i < weatherNameLibrary.length; i++){
 		if (weatherNames[weatherNameLibrary[i]] == undefined) {
 			weatherNames[weatherNameLibrary[i]] = 0;
@@ -184,47 +197,57 @@ function setup() {
 		}
 	}
 	
-	var ind = 0;
-	var xCoord = 1200;
-	var yCoord = 75;
+	var ind = 0; // index for inserting in buttonArray
+	var xCoord = 1200; // initial x-coordinate
+	var yCoord = 75; // initial y-coordinate
+	
+	// creates new button for each weather description in the dictionary, which at this point hold all known weather descriptions,
+	// as well as any new/unknown weather that is currently happening at any of the cities
 	for (var key in weatherNames){
   	var value = weatherNames[key];
-		buttonArray[ind] = new Button(xCoord, yCoord, key, value);
+		buttonArray[ind] = new Button(xCoord, yCoord, key, value); 
 		ind += 1;
 		yCoord += 30;
 	}
 }
 
 // DRAW LOOP //
-
 function draw() {
 	fill(skyColor);
 	square(0, 0, width);
 	image(img, 0, 0, 1131, 724);
 	fill(0);
 	
+	// display all buttons
 	for (i = 0; i < buttonArray.length; i++){
 		buttonArray[i].display();
 	}
 	
+	// if a button is active, display the weather for that button
 	for (i = 0; i < buttonArray.length; i++){
 		if (buttonArray[i].isActive == true){
 			buttonArray[i].displayWeather();
+			break;
 		}
 	}
 	
+	// display all cities
 	for (i = 0; i < cityArray.length; i++){
+		// this conditional fixes a small bug where city dots would not show on load
 		if (activeCity == 0){
 			cityArray[i].display();
 		}
+		// if city is actve, display the weather for that city
 		if (cityArray[i].isActive == true){
-			activeCity = cityArray[i];
-			activeCity.display();
+			activeCity = 1
+			cityArray[i].display();
 			cityArray[i].displayWeather();
+			break;
 		}
 	}
 }
 
+// if mouse is pressed, run mouseClick functions for all city dots and all buttons to see if it was clicked
 function mousePressed(){
 	for (i = 0; i < cityArray.length; i++){
 		cityArray[i].mouseClick();
@@ -232,7 +255,7 @@ function mousePressed(){
 	for (i = 0; i < buttonArray.length; i++){
 		buttonArray[i].mouseClick();
 	}
-	 print("X: " + mouseX + " Y: " + mouseY);
+	//print("X: " + mouseX + " Y: " + mouseY);
 }
 
 
@@ -241,7 +264,6 @@ function mousePressed(){
 // LOCATION CLASS //
 class Location {
 	constructor(cityJSON, xCoord, yCoord) {
-		this.test = 5;
 		this.name = cityJSON.name;
 		this.x = xCoord;
 		this.y = yCoord;
@@ -253,6 +275,7 @@ class Location {
 		this.displayCase = "";
   }
 	
+	// if button was clicked, make it active, otherwise make it inactive
 	mouseClick(){
 		if (dist(this.x, this.y, mouseX, mouseY) < 12) {
 			if (this.isActive == false) {
@@ -267,6 +290,7 @@ class Location {
 		
 	displayWeather(){
 		
+		// display info from JSON
 		strokeWeight(1);
 		stroke(2);
 		text("City: " + this.name, this.x - 10, this.y + 25);
@@ -274,31 +298,36 @@ class Location {
 		text("Forecast: " + this.weather, this.x - 10, this.y + 65);
 		text("Wind: " + this.windSpeed + "mph", this.x - 10, this.y + 85);
 		
+		// conditionals for displaying weather based on weather description
 		if (this.weather == "clear sky"){
 			weatherClassObj.clearSky();
 		} else if (this.weather == "scattered clouds"){
-			// weatherClassObj.clouds("scattered clouds");
+			weatherClassObj.clouds("scattered cloud");
 		} else if (this.weather == "broken clouds"){
-			//weatherClassObj.clouds("broken clouds");
+			weatherClassObj.clouds("broken clouds");
 		} else if (this.weather == "overcast clouds"){
-			//weatherClassObj.clouds("overcast clouds");
+			weatherClassObj.clouds("overcast clouds");
 		} else if (this.weather == "few clouds"){
-			//weatherClassObj.clouds("few clouds");
+			weatherClassObj.clouds("few clouds");
 		} else if (this.weather == "light rain"){
-			//weatherClassObj.rain("light rain");
+			weatherClassObj.rain("light rain");
 		} else if (this.weather == "light intensity drizzle"){
-			//weatherClassObj.rain("light rain");
+			weatherClassObj.rain("light rain");
 		} else if (this.weather == "shower rain"){
-			//weatherClassObj.rain("shower rain");
+			weatherClassObj.rain("shower rain");
 		} else if (this.weather == "very heavy rain"){
-			//weatherClassObj.rain("heavy rain");
+			weatherClassObj.rain("heavy rain");
 		} else if (this.weather == "moderate rain"){
-			//weatherClassObj.rain("moderate rain");
+			weatherClassObj.rain("moderate rain");
 		} else if (this.weather == "heavy intensity rain"){
-			//weatherClassObj.rain("heavy intensity rain");
+			weatherClassObj.rain("heavy intensity rain");
+		} else if (this.weather == "light intensity rain"){
+			weatherClassObj.rain("light rain");
 		} else if (this.weather == "haze"){
 			weatherClassObj.haze();
 		} else if (this.weather == "mist"){
+			weatherClassObj.haze();
+		} else if (this.weather == "smoke"){
 			weatherClassObj.haze();
 		} else if (this.weather == "thunderstorm with rain"){
 			weatherClassObj.thunderstorm();
@@ -312,36 +341,42 @@ class Location {
 
 class Weather {
 	constructor() {
-		this.hazeOpacity = 50;
+		this.hazeOpacity = 50; // opacity variable for haze/fog to change appearance
 		this.hazeFogging = true;
 		this.lightningOpacity = 0;
 	}
 	
+	// methods for showing weather
+	
+	// weather shown for the different types of rain
 	rain(rainType){
 		if (rainType == "light rain"){
-			for (i = 0; i < 25; i++){
+			for (i = 0; i < 25; i++){ // some raindrops shown
 				fastDropArray[i].show();
 				fastDropArray[i].fall();
 			}
 		} else if (rainType == "moderate rain"){
-			for (i = 0; i < 75; i++){
+			for (i = 0; i < 75; i++){ // more raindrops shown
 				fastDropArray[i].show();
 				fastDropArray[i].fall();
 			}
 		} else if (rainType == "heavy rain"){
-			for (i = 0; i < fastDropArray.length; i++){
+			for (i = 0; i < fastDropArray.length; i++){ // all raindrops shown
 				fastDropArray[i].show();
 				fastDropArray[i].fall();
 			}
 		} else {
-			for (i = 0; i < 250; i++){
+			for (i = 0; i < 250; i++){ // default num of raindrops shown
 				fastDropArray[i].show();
 				fastDropArray[i].fall();
 			}
 		}
 	}
 	
+	// weather shown for "clear sky" weather description
 	clearSky() {
+		// create and rotate sun graphic
+		// code for rotation from https://editor.p5js.org/monicawen/sketches/HkU-BCJqm
 		skyColor = color(148, 214, 255);
 		fill(245, 212, 47);
 		stroke(245, 187, 87);
@@ -362,40 +397,42 @@ class Weather {
 		noFill();
 	}
 	
+	// weather shown for different types of clouds
 	clouds(cloudType){
 		skyColor = color(148, 214, 255);
 		if (cloudType == "scattered clouds"){
-			for (i = 0; i < 5; i++){
+			for (i = 0; i < 5; i++){ // show several clouds
 				cloudArray[i].show();
 				cloudArray[i].move(true);
 			}
 			
 		} else if (cloudType == "broken clouds"){
-			for (i = 0; i < 10; i++){
+			for (i = 0; i < 10; i++){ // show some clouds
 				cloudArray[i].show();
 				cloudArray[i].move(true);
 			}
 		} else if (cloudType == "few clouds"){
-			for (i = 0; i < 3; i++){
+			for (i = 0; i < 3; i++){ // show a few clouds
 				cloudArray[i].show();
 				cloudArray[i].move(true);
 			}
 		} else if (cloudType == "overcast clouds"){
-			skyColor = color(151, 170, 186);
-			for (i = 0; i < cloudArray.length; i++){
+			skyColor = color(151, 170, 186); // darken sky
+			for (i = 0; i < cloudArray.length; i++){ // show all clouds, not moving (overcast)
 				cloudArray[i].show();
 				cloudArray[i].move(false);
 			}
 		} else {
-			for (i = 0; i < 10; i++){
+			for (i = 0; i < 10; i++){ // default amount of clouds
 				cloudArray[i].show();
 				cloudArray[i].move();
 			}
 		}
 	}	
 	
+	// weather shown for "haze" and "mist" weather descriptions
 	haze(){
-		
+		// alpha value of gray rectangle bounces between 25-100 to create cool effect
 		fill(194, 194, 194, this.hazeOpacity);
 		rect(0, 0, windowWidth, windowHeight);
 		if (this.hazeFogging){
@@ -411,22 +448,27 @@ class Weather {
 		}
 	}
 	
+	// weather shown for "snow" weather descriptions. it never snowed anywhere while I created this program
+	// so I'm not sure the even description, although it is probaly something similar
+	// code is from: https://p5js.org/examples/simulate-snowflakes.html
 	snow(){
-			let t = frameCount / 60; // update time
+			let t = frameCount / 60; // update time based on the current frame count
 
-			// create a random number of snowflakes each frame
+			// creates a random number (between 1-5) of Snowflake objects for each frame in range
 			for (let i = 0; i < random(5); i++) {
-				snowflakeArray.push(new Snowflake()); // append snowflake object
+				snowflakeArray.push(new Snowflake()); // add Snowflake objects to snowflakeArray
 			}
 
-			// loop through snowflakes with a for..of loop
+			// loop through snowflakeArray, show them, then update them
 			for (i = 0; i < snowflakeArray.length; i++) {
-				snowflakeArray[i].update(t); // update snowflake position
-				snowflakeArray[i].display(); // draw snowflake
+				snowflakeArray[i].update(t); // update Snowflake position
+				snowflakeArray[i].display(); // show Snowflake
 			}
 	}
 	
+	// weather shown for "thunderstorm with rain" weather description
 	thunderstorm(){
+		// code for a lightning bolt
 		// fill(color("yellow"));
 		// let randomVal = random(wid);
 		// height = 400
@@ -434,6 +476,7 @@ class Weather {
 		// rect(randomVal, randomVal - 30, 100, 40);
 		// triangle(randomVal + 25, randomVal - 30, randomVal + 125, randomVal - 30, randomVal + 25, randomVal + 200);
 		
+		// will create a 2 second timer, every two seconds there will be a quick "flash" of lightning where the alpha value is raised temporarily
 		fill(194, 194, 194, this.lightningOpacity);
 		this.lightningOpacity = 0;
 		rect(0, 0, windowWidth, windowHeight);
@@ -445,19 +488,17 @@ class Weather {
 			lightningTimer = 2;
   	}
 		
-		
+		// code for making it rain at the same time
 		for (i = 0; i < fastDropArray.length; i++){
-				fastDropArray[i].show();
-				fastDropArray[i].fall();
+				fastDropArray[i].show(); // show rain
+				fastDropArray[i].fall(); // update rain
 			}
 	}
-	
-	
-	
 }
 	
 // CITY CLASS //
 
+// city class inherits from Location class
 class City extends Location {
 	constructor(cityJSON, xCoord, yCoord){
 		super(cityJSON, xCoord, yCoord);																									
@@ -467,6 +508,7 @@ class City extends Location {
 		
 		noStroke();
 		
+		// conditionals for basing city dot on current temperature there
 		if (this.temp > 100){
 			fill("#FC582A")
 		} else if (this.temp > 80) {
@@ -503,12 +545,14 @@ class Raindrop {
 		this.speed = random(4,10);
 	}
 	
+	// draws the Raindrop
 	show(){
 		stroke(23, 87, 156);
 		strokeWeight(10)
 		line(this.xCoord, this.yCoord, this.xCoord, this.yCoord + 10);
 	}
 	
+	// updates the Raindrop's position
 	fall(){
 		this.yCoord += this.speed;
 		if (this.yCoord > height){
@@ -525,6 +569,8 @@ class Cloud {
 		this.y = random(height/5);
 		this.speed = random(1,3);
 	}
+	
+	// draws the Cloud
 	show(){
 		noStroke();
 		fill(255);
@@ -535,6 +581,7 @@ class Cloud {
 		ellipse(this.x + 60, this.y + 15, 60, 50);
 	}
 	
+	// updates the Cloud's position if it is meant to move (only overcast does not move)
 	move(boolVal){
 		if (boolVal == true){
 			this.x += this.speed;
@@ -551,31 +598,25 @@ class Snowflake {
   constructor(){
 		this.posX = 0;
 		this.posY = random(-50, 0);
-		this.initialangle = random(0, 2 * PI);
-		this.size = random(2, 5);
-		this.radius = sqrt(random(pow(width / 2, 2)));
+		this.initialangle = random(0, 2 * PI); // intial angle that snowflake moves relative to ground
+		this.size = random(2, 5); // creates random radius size for Snowflake objects
+		this.radius = sqrt(random(pow(width / 2, 2))); // determines path of snowflake with respect ot the side of the screen
 	}
 
-  // radius of snowflake spiral
-  // chosen so the snowflakes are uniformly spread out in area
-  
-
+	// updates position of the Snowflake object
   update(time) {
     // x position follows a circle
     let w = 0.6; // angular speed
     let angle = w * time + this.initialangle;
     this.posX = width / 2 + this.radius * sin(angle);
-
-    // different size snowflakes fall at slightly different y speeds
-    this.posY += pow(this.size, 0.5);
-
-    // delete snowflake if past end of screen
-    if (this.posY > height) {
+    this.posY += pow(this.size, 0.5); // different size snowflakes fall at slightly different y speeds
+		// delete Snowflake objects if it foes past the end of window with
+		if (this.posY > height) {
       let index = snowflakeArray.indexOf(this);
       snowflakeArray.splice(index, 1);
     }
   }
-
+	// draw snowflake (as circle)
   display() {
     ellipse(this.posX, this.posY, this.size);
   }
@@ -593,8 +634,10 @@ class Button {
 		this.isActive = false;
 	}
 	
+	// draws buttons
 	display(){
 		noStroke();
+		// if button is inactive it is black, if it is active is it white
 		if (this.isActive == false){
 			fill(255);
 		} else {
@@ -606,6 +649,7 @@ class Button {
 		skyColor = color(125);
 	}
 	
+	// conditionals for showing the weather based on what button was pressed
 	displayWeather(){
 		if (this.weatherDescription == "clear sky"){
 			weatherClassObj.clearSky();
@@ -624,24 +668,29 @@ class Button {
 		} else if (this.weatherDescription == "very heavy rain"){
 			weatherClassObj.rain("heavy rain");
 		} else if (this.weatherDescription == "moderate rain"){
-			weatherClassObj.rain("moderate rain");
+			weatherClassObj.rain("shower rain");
 		} else if (this.weatherDescription == "heavy intensity rain"){
 			weatherClassObj.rain();
-		} else if (this.weatherDescription == "haze"){
-			weatherClassObj.haze();
+		} else if (this.weatherDescription == "heavy intensity rain"){
+			weatherClassObj.rain();
+		} else if (this.weatherDescription == "light intensity rain"){
+			weatherClassObj.rain("light rain");
 		} else if (this.weatherDescription == "mist"){
+			weatherClassObj.haze();
+		} else if (this.weatherDescription == "haze"){
 			weatherClassObj.haze();
 		} else if (this.weatherDescription == "thunderstorm with rain"){
 			weatherClassObj.thunderstorm();
 		} else if (this.weatherDescription == "snow"){
 			weatherClassObj.snow();
-		}
-		
-		else {
+		}	else if (this.weatherDescription == "smoke"){
+			print("add smoking display");
+		}	else {
 			print("weather is undefined");
 		}
 	}
 	
+	// check if button was pressed, update isActive status accordingly
 	mouseClick(){
 		if (dist(this.xCoord, this.yCoord, mouseX, mouseY) < 12) {
 			if (this.isActive == false) {
